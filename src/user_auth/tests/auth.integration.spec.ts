a82685aacddbc7047dc871d530c8eeccbc21bfef
trongtext <hiiamtrong@gmail.com>
2022-08-08 16:55:48 +0700

    finish auth integration testing

--- src/user_auth/tests/auth.integration.spec.ts
+++ src/user_auth/tests/auth.integration.spec.ts
@@ -99,4 +99,71 @@ describe('auth.integration.spec.ts', () => {
       await expect(authController.login(form)).rejects.toThrowError(error);
     });
   });
+
+  describe('register', () => {
+    it('Should return verify token', async () => {
+      const form = {
+        email: 'user3@gmail.com',
+        password: '123456',
+      };
+
+      const res = await authController.register(form);
+
+      expect(res.data.verify_token).toBeDefined();
+      expect(res.statusCode).toBe(200);
+      expect(res.message).toBe('success');
+
+      const token = await redisService.client.get(
+        `verifyToken:${res.data.verify_token}`,
+      );
+      expect(token).toBeTruthy();
+    });
+
+    it('Should throw error when email already exists', async () => {
+      const error = new Error('Email already exists');
+
+      const form = {
+        email: 'user1@gmail.com',
+        password: '123456',
+      };
+
+      await expect(authController.register(form)).rejects.toThrowError(error);
+    });
+  });
+
+  describe('verifyRegister', () => {
+    it('Should create new user_auth', async () => {
+      const form = {
+        email: 'user3@gmail.com',
+        password: '123456',
+      };
+
+      const resRegister = await authController.register(form);
+
+      const verifyToken = await redisService.client.get(
+        `verifyToken:${resRegister.data.verify_token}`,
+      );
+
+      expect(verifyToken).toBeTruthy();
+
+      const resVerify = await authController.verifyRegister(verifyToken);
+
+      expect(resVerify.statusCode).toBe(200);
+
+      const userAuth = await prismaService.userAuth.findFirst({
+        where: { email: form.email },
+      });
+      expect(userAuth).toBeTruthy();
+      expect(userAuth.isAdmin).toBe(false);
+      expect(userAuth.roles).toEqual(['USER']);
+    });
+
+    it('Should throw error when wrong token', async () => {
+      const error = new Error('Invalid token');
+
+      expect(authController.verifyRegister('wrong token')).rejects.toThrowError(
+        error,
+      );
+    });
+  });
 });
